ee61f773abfb9e66ec1fb9c1c192d2de8498f90c
David Vaz Cortesão <davidvazc@gmail.com>
2019-10-24 12:40:57 +0100

    funçoes para indexar e indexar recursivamente

--- meta1/src/ucBuscas/WebCrawler.java
+++ meta1/src/ucBuscas/WebCrawler.java
@@ -1,22 +1,163 @@
 package ucBuscas;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class WebCrawler {
-    private static final int N_paginas_A_visitar = 10;
+    private static final int N_paginas_A_visitar = 2;
     private Set<String> paginasVisitadas = new HashSet<String>();
     private List<String> paginas_A_Visitar = new LinkedList<String>();
+    private static List<String> links2 = new LinkedList<String>();
+
+
+    public static void main(String args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        Document doc =null;
+
+
+        // Read website
+        System.out.print("Website: ");
+        try {
+
+            String ws = bf.readLine();
+            if (! ws.startsWith("http://") && ! ws.startsWith("https://"))
+                ws = "http://".concat(ws);
+
+
+            try {
+                // Attempt to connect and get the document
+                doc = Jsoup.connect(ws).get();  // Documentation: https://jsoup.org/
+            }
+            catch (MalformedURLException e){
+                System.out.println("**Failure** Retrieved something other than HTML");
+            }
+
+            // Title
+            System.out.println(doc.title() + "\n");
+
+            // Get all links
+            Elements links = doc.select("a[href]");
+            for (Element link : links) {
+                // Ignore bookmarks within the page
+                if (link.attr("href").startsWith("#")) {
+                    continue;
+                }
+
+                // Shall we ignore local links? Otherwise we have to rebuild them for future parsing
+                if (!link.attr("href").startsWith("http")) {
+                    continue;
+                }
+                links2.add(link.absUrl("href"));
+                System.out.println("Link: " + link.attr("href"));
+                System.out.println("Text: " + link.text() + "\n");
+            }
+
+            // Get website text and count words
+            String text = doc.text(); // We can use doc.body().text() if we only want to get text from <body></body>
+            countWords(text);
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("**Failure** Retrieving links!");
+        }
+    }
+
+    private static void countWords(String text) {
+        Map<String, Integer> countMap = new TreeMap<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8))));
+        String line;
+
+        // Get words and respective count
+        while (true) {
+            try {
+                if ((line = reader.readLine()) == null)
+                    break;
+                String[] words = line.split("[ ,;:.?!“”(){}\\[\\]<>']+");
+                for (String word : words) {
+                    word = word.toLowerCase();
+                    if ("".equals(word)) {
+                        continue;
+                    }
+                    if (!countMap.containsKey(word)) {
+                        countMap.put(word, 1);
+                    }
+                    else {
+                        countMap.put(word, countMap.get(word) + 1);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Close reader
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Display words and counts
+        for (String word : countMap.keySet()) {
+            if (word.length() >= 3) { // Shall we ignore small words?
+                System.out.println(word + "\t" + countMap.get(word));
+            }
+        }
+    }
+
+    public List<String> getLinks() {
+
+        return this.links2;
+    }
+
+
+    public void indexaRecursiva(String url){
+        System.out.print("Website: ");
+        String ws =url ;
+        if (! ws.startsWith("http://") && ! ws.startsWith("https://"))
+            ws = "http://".concat(ws);
+
+        while(this.paginasVisitadas.size() < N_paginas_A_visitar) {
+            String currentUrl;
+            if(this.paginas_A_Visitar.isEmpty()) {
+                currentUrl = ws;
+                this.paginasVisitadas.add(ws);
+            }
+            else {
+                currentUrl = this.nextUrl();
+            }
+            main(currentUrl);
+            this.paginas_A_Visitar.addAll(this.getLinks());
+        }
+        System.out.println("\n**Done** Visited " + this.paginasVisitadas.size() + " web page(s)");
+
+    }
+
 
     /**
-     * aqui metemos o URL e a palavra, ainda tenho de fazer um construtor p receber so o url
-     * ou só a palavra
-     * @param url
-     * @param searchWord
+     * função que vai percorrendo a lista de sites a usar e removendo-os da lista
+     * @return
      */
+    private String nextUrl() {
+        String nextUrl;
+        do {
+            nextUrl = this.paginas_A_Visitar.remove(0);
+        } while(this.paginasVisitadas.contains(nextUrl));
+        this.paginasVisitadas.add(nextUrl);
+        return nextUrl;
+    }
+}
 
+/*
     public void search(String url, String searchWord)
     {
         while(this.paginasVisitadas.size() < N_paginas_A_visitar)
@@ -44,19 +185,4 @@ public class WebCrawler {
         System.out.println("\n**Done** Visited " + this.paginasVisitadas.size() + " web page(s)");
     }
 
-
-    /**
-     * função que vai percorrendo a lista de sites a usar e removendo-os da lista
-     * @return
-     */
-    private String nextUrl()
-    {
-        String nextUrl;
-        do
-        {
-            nextUrl = this.paginas_A_Visitar.remove(0);
-        } while(this.paginasVisitadas.contains(nextUrl));
-        this.paginasVisitadas.add(nextUrl);
-        return nextUrl;
-    }
-}
+ */
--- meta1/src/ucBuscas/WebCrawler2.java
+++ meta1/src/ucBuscas/WebCrawler2.java
@@ -20,51 +20,6 @@ public class WebCrawler2 {
     private List<String> links = new LinkedList<String>();
     private Document htmlDocument;
 
-
-    /**
-     * Esta funçao foi dada pelo prof, o file json
-     * @param args
-     */
-    public static void main(String[] args) {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        // Read website
-        System.out.print("Website: ");
-        try {
-            String ws = bf.readLine();
-            if (! ws.startsWith("http://") && ! ws.startsWith("https://"))
-                ws = "http://".concat(ws);
-
-            // Attempt to connect and get the document
-            Document doc = Jsoup.connect(ws).get();  // Documentation: https://jsoup.org/
-
-            // Title
-            System.out.println(doc.title() + "\n");
-
-            // Get all links
-            Elements links = doc.select("a[href]");
-            for (Element link : links) {
-                // Ignore bookmarks within the page
-                if (link.attr("href").startsWith("#")) {
-                    continue;
-                }
-
-                // Shall we ignore local links? Otherwise we have to rebuild them for future parsing
-                if (!link.attr("href").startsWith("http")) {
-                    continue;
-                }
-
-                System.out.println("Link: " + link.attr("href"));
-                System.out.println("Text: " + link.text() + "\n");
-            }
-
-            // Get website text and count words
-            String text = doc.text(); // We can use doc.body().text() if we only want to get text from <body></body>
-            countWords(text);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
     /**
      * Faz request Http, ve a resposta, ve os links e pesquisa as palavras
      *
@@ -72,50 +27,41 @@ public class WebCrawler2 {
      * @return False se não tiver conseguido
      */
 
-    public boolean crawl(String url)
-    {
-        try
-        {
+    public boolean crawl(String url) {
+        try {
             Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
             Document htmlDocument = connection.get();
             this.htmlDocument = htmlDocument;
-            if(connection.response().statusCode() == 200) // 200 is the HTTP OK status code
+            if (connection.response().statusCode() == 200) // 200 is the HTTP OK status code
             // indicating that everything is great.
             {
                 System.out.println("\n**Visiting** Received web page at " + url);
             }
-            if(!connection.response().contentType().contains("text/html"))
-            {
+            if (!connection.response().contentType().contains("text/html")) {
                 System.out.println("**Failure** Retrieved something other than HTML");
                 return false;
             }
             Elements linksOnPage = htmlDocument.select("a[href]");
             System.out.println("Found (" + linksOnPage.size() + ") links");
-            for(Element link : linksOnPage)
-            {
+            for (Element link : linksOnPage) {
                 this.links.add(link.absUrl("href"));
             }
             return true;
-        }
-        catch(IOException ioe)
-        {
+        } catch (IOException ioe) {
             // We were not successful in our HTTP request
             return false;
         }
     }
 
     /**
-     *
      * Depois do crawl ele pesquisa a palavra no documento html
      *
      * @param searchWord
      */
 
-    public boolean searchForWord(String searchWord)
-    {
+    public boolean searchForWord(String searchWord) {
         // Defensive coding. This method should only be used after a successful crawl.
-        if(this.htmlDocument == null)
-        {
+        if (this.htmlDocument == null) {
             System.out.println("ERROR! Call crawl() before performing analysis on the document");
             return false;
         }
@@ -123,57 +69,8 @@ public class WebCrawler2 {
         String bodyText = this.htmlDocument.body().text();
         return bodyText.toLowerCase().contains(searchWord.toLowerCase());
     }
-
-
-    public List<String> getLinks()
-    {
-        return this.links;
-    }
-
-    /**
-     * Este tambem foi dado pelo prof, é a continuação do incial
-     * @param text
-     */
-    private static void countWords(String text) {
-        Map<String, Integer> countMap = new TreeMap<>();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8))));
-        String line;
-
-        // Get words and respective count
-        while (true) {
-            try {
-                if ((line = reader.readLine()) == null)
-                    break;
-                String[] words = line.split("[ ,;:.?!“”(){}\\[\\]<>']+");
-                for (String word : words) {
-                    word = word.toLowerCase();
-                    if ("".equals(word)) {
-                        continue;
-                    }
-                    if (!countMap.containsKey(word)) {
-                        countMap.put(word, 1);
-                    }
-                    else {
-                        countMap.put(word, countMap.get(word) + 1);
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        // Close reader
-        try {
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Display words and counts
-        for (String word : countMap.keySet()) {
-            if (word.length() >= 3) { // Shall we ignore small words?
-                System.out.println(word + "\t" + countMap.get(word));
-            }
-        }
-    }
 }
+
+
+
+
